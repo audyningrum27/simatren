41197531769a56d77c8031f36cc7b206df9fda87
LaelaAnggraeni08 <anggraenilaila64@gmail.com>
2024-06-02 20:58:01 +0700

    edit bukti pelatihan

--- src/AdminPage/components/pages/Manajemen-Pelatihan/DetailHistoryPelatihan.jsx
+++ src/AdminPage/components/pages/Manajemen-Pelatihan/DetailHistoryPelatihan.jsx
@@ -1,16 +1,18 @@
+import React from "react";
 import { getPegawaiStatus } from "../../utils/status";
+import { MdImage } from "react-icons/md";
 
 const detailPegawai = [
   {
     id: '1',
     nip: '19860926201500',
-    nama: 'Albert Eisten',
+    nama: 'Albert Einstein',
     tgl_mulai: '2024-05-21T05:24:00',
     tgl_selesai: '2024-05-22T05:24:00',
     nama_kegiatan: 'Pelatihan Mengajar',
-    deskrisi_kegiatan: 'Lorem Ipsum is simply dummy text.',
+    deskripsi_kegiatan: 'Lorem Ipsum is simply dummy text.',
     status: 'SELESAI',
-    bukti: '-',
+    bukti: 'Bukti Foto'
   }
 ]
 
@@ -26,67 +28,67 @@ export default function DetailHistoryPelatihan() {
               <table className='w-full border-separate p-5 text-gray-950 text-sm'>
                 <tbody>
                   {detailPegawai.map((pelatihan) => (
-                  <tr key={pelatihan.id}>
-                    <td>Nomor Induk Pegawai</td>
-                    <td className="p-2">:</td>
-                    <td className="px-2 border border-gray-400 rounded-md">{pelatihan.nip}</td>
-                  </tr>
-                  ))}
-                  {detailPegawai.map((pelatihan) => (
-                  <tr key={pelatihan.id}>
-                    <td >Nama Pegawai</td>
-                    <td className="p-2">:</td>
-                    <td className="px-2 border border-gray-400 rounded-md">{pelatihan.nama}</td>
-                  </tr>
-                  ))}
-                  {detailPegawai.map((pelatihan) => (
-                  <tr key={pelatihan.id}>
-                    <td>Tanggal Mulai</td>
-                    <td className="p-2">:</td>
-                    <td className="px-2 border border-gray-400 rounded-md">{new Date(pelatihan.tgl_mulai).toLocaleDateString()}</td>
-                  </tr>
-                  ))}
-                  {detailPegawai.map((pelatihan) => (
-                  <tr key={pelatihan.id}>
-                    <td>Tanggal Selesai</td>
-                    <td className="p-2">:</td>
-                    <td className="px-2 border border-gray-400 rounded-md">{new Date(pelatihan.tgl_selesai).toLocaleDateString()}</td>
-                  </tr>
-                  ))}
-                  {detailPegawai.map((pelatihan) => (
-                  <tr key={pelatihan.id}>
-                    <td>Nama Kegiatan</td>
-                    <td className="p-2">:</td>
-                    <td className="px-2 border border-gray-400 rounded-md">{pelatihan.nama_kegiatan}</td>
-                  </tr>
-                  ))}
-                  {detailPegawai.map((pelatihan) => (
-                  <tr key={pelatihan.id}>
-                    <td>Deskripsi Kegiatan</td>
-                    <td className="p-2">:</td>
-                    <td className="px-2 border border-gray-400 rounded-md">{pelatihan.deskrisi_kegiatan}</td>
-                  </tr>
-                  ))}
-                  {detailPegawai.map((pelatihan) => (
-                  <tr key={pelatihan.id}>
-                    <td>Status</td>
-                    <td className="p-2">:</td>
-                    <td>{getPegawaiStatus(pelatihan.status)}</td>
-                  </tr>
-                  ))}
-                  {detailPegawai.map((pelatihan) => (
-                  <tr key={pelatihan.id}>
-                    <td>Bukti Pelaksanaan</td>
-                    <td className="p-2">:</td>
-                    <td>{pelatihan.bukti}</td>
-                  </tr>
+                    <React.Fragment key={pelatihan.id}>
+                      <tr>
+                        <td>Nomor Induk Pegawai</td>
+                        <td className="p-2">:</td>
+                        <td className="px-2 border border-gray-400 rounded-md">{pelatihan.nip}</td>
+                      </tr>
+                      <tr>
+                        <td>Nama Pegawai</td>
+                        <td className="p-2">:</td>
+                        <td className="px-2 border border-gray-400 rounded-md">{pelatihan.nama}</td>
+                      </tr>
+                      <tr>
+                        <td>Tanggal Mulai</td>
+                        <td className="p-2">:</td>
+                        <td className="px-2 border border-gray-400 rounded-md">{new Date(pelatihan.tgl_mulai).toLocaleDateString()}</td>
+                      </tr>
+                      <tr>
+                        <td>Tanggal Selesai</td>
+                        <td className="p-2">:</td>
+                        <td className="px-2 border border-gray-400 rounded-md">{new Date(pelatihan.tgl_selesai).toLocaleDateString()}</td>
+                      </tr>
+                      <tr>
+                        <td>Nama Kegiatan</td>
+                        <td className="p-2">:</td>
+                        <td className="px-2 border border-gray-400 rounded-md">{pelatihan.nama_kegiatan}</td>
+                      </tr>
+                      <tr>
+                        <td>Deskripsi Kegiatan</td>
+                        <td className="p-2">:</td>
+                        <td className="px-2 border border-gray-400 rounded-md">{pelatihan.deskripsi_kegiatan}</td>
+                      </tr>
+                      <tr>
+                        <td>Status</td>
+                        <td className="p-2">:</td>
+                        <td>{getPegawaiStatus(pelatihan.status)}</td>
+                      </tr>
+                      <tr>
+                        <td>Bukti Pelaksanaan</td>
+                        <td className="p-2">:</td>
+                        <td className="flex items-center">
+                          <div className="flex space-x-4">
+                            <div className="w-16 h-16 border border-gray-400 text-gray-400 flex justify-center items-center">
+                              <MdImage fontSize={72} />
+                            </div>
+                            <div className="w-16 h-16 border border-gray-400 text-gray-400 flex justify-center items-center">
+                              <MdImage fontSize={72} />
+                            </div>
+                            <div className="w-16 h-16 border border-gray-400 text-gray-400 flex justify-center items-center">
+                              <MdImage fontSize={72} />
+                            </div>
+                          </div>
+                        </td>
+                      </tr>
+                    </React.Fragment>
                   ))}
                 </tbody>
               </table>
             </div>
           </div>
         </div>
-      </div>   
+      </div>
     </div>
-  )
+  );
 }
